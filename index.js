8f23623efaed33883b5daa5bd787df608d9f3af4
Mr. Kutin <serge.kutin@gmail.com>
2023-01-22 22:55:26 +0300

    autoins

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // puppeteer-extra is a drop-in replacement for puppeteer,
 // it augments the installed puppeteer with plugin functionality
-const { executablePath } = require('puppeteer')
+const {executablePath} = require('puppeteer')
 const puppeteer = require('puppeteer-extra')
 
 // const SessionPlugin = require('puppeteer-extra-plugin-session').default()
@@ -13,7 +13,7 @@ puppeteer.use(SessionPlugin.default())
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin())
 
-const { scrollPageToBottom, scrollPageToTop } = require('puppeteer-autoscroll-down')
+const {scrollPageToBottom, scrollPageToTop} = require('puppeteer-autoscroll-down')
 
 // puppeteer usage as normal
 puppeteer.launch({
@@ -42,6 +42,25 @@ puppeteer.launch({
     await page.click('#buttonFind')
     await page.waitForNavigation({waitUntil: 'networkidle2'})
 
+    //todo const texts = await page.evaluate(() => Array.from(document.querySelectorAll('tr.data-row > td')).map(el => el.innerText))
+    const headers = ['№', 'Серия и номер договора ОСАГО', 'Наименование страховой организации', 'Статус договора ОСАГО', 'Срок действия и период использования транспортного средства договора ОСАГО', 'Сведения о транспортном средстве', 'Транспортное средство следует к месту регистрации или к месту проведения технического осмотра', 'Управление транспортным средством с прицепом', 'Цель использования транспортного средства', 'Договор ОСАГО с ограничениями/без ограничений лиц, допущенных к управлению транспортным средством', 'Сведения о страхователе транспортного средства', 'Сведения о собственнике транспортного средства', 'КБМ по договору ОСАГО', 'Транспортное средство используется в регионе', 'Страховая премия']
+    const texts = [
+        "1",
+        "ХХХ 0226901657",
+        "САО \"ВСК\"",
+        "Действует",
+        "Период использования ТС активен на запрашиваемую дату",
+        "Марка и модель транспортного средства (категория \"Х\")\tLexus IS (категория «B»)\nГосударственный регистрационный знак\tХ290КК777\nVIN\tJTHBK262402022481\nМощность двигателя для категории B, л.с.\t208.00",
+        "Нет",
+        "Нет",
+        "Личная",
+        "Ограничен список лиц, допущенных к управлению (допущено: 3 чел.)",
+        "М***** АЛЕКСЕЙ СЕРГЕЕВИЧ 19.06.1991",
+        "М***** АЛЕКСЕЙ СЕРГЕЕВИЧ 19.06.1991",
+        "0.65",
+        "Оренбургская обл, г Оренбург",
+        "7788.38 руб."
+    ]
     console.log()
 
     // const sessionData = await page.session.dump()
